5eebf67306faa3501f8f3bc7fc3ab7ad741fd6f7
+ <+>
2022-01-07 15:01:42 -0500

    navbar updated, home page updated

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,22 +3,47 @@
 //Node getters
 const mainDiv = () => document.getElementById('main')
 const homeLink = () => document.getElementById('home-link')
+const formDiv = () => document.getElementById('form')
+
 //Event Listeners
 
-//Event Handlers
-const loadHome = () => {
-    const h1 = document.createElement('h1')
-
-    h1.className = "center-align"
-    h1.innerText = 'Your List'
-    
-    mainDiv().appendChild(h1)
-    
+const attachHomePageLinkEvent = () => {
+    homeLink().addEventListener('click', loadHome)
 }
 
+
+
+//Event Handlers
+
+const loadHome = () => {
+    resetMainDiv()
+    const h1 = document.createElement('h1')
+    const h4 = document.createElement('h4')
+
+    h1.className = "center-align"
+    h4.className = "center-align"
+    h1.innerText = 'Welcome to Listless'
+    h4.innerText = "Listless allows you to sort your shopping list into categories, so you won't leave the dairy section without grabbing oat milk ever again!"
+
+    
+    mainDiv().appendChild(h1)
+    mainDiv().appendChild(h4)
+}
+
+const loadForm = () => {
+    document.createElement('form')
+
+}
+
+//Misc
+
+const resetMainDiv = () => {
+    mainDiv().innerHTML = ''
+}
 
 //WHEN THE DOM LOADS
 document.addEventListener('DOMContentLoaded', function(){
     //load the home page
     loadHome()
+    attachHomePageLinkEvent()
 })
