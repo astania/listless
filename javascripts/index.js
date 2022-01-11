202552574d9978c58264f769ed6e44192368bcd3
+ <+>
2022-01-11 12:33:23 -0500

    completed input form. added submit button

--- javascripts/index.js
+++ javascripts/index.js
@@ -23,7 +23,7 @@ const attachMealLinkEvent = () => {
 //Event Handlers
 
 const loadHome = event => {
-    if(event){
+    if (event) {
         event.preventDefault()
     }
     resetMainDiv()
@@ -35,7 +35,7 @@ const loadHome = event => {
     h1.innerText = 'Welcome to LISTLESS'
     h4.innerText = "Listless allows you to sort your shopping list into categories, so you won't leave the dairy section without grabbing oat milk ever again!"
 
-    
+
     mainDiv().appendChild(h1)
     mainDiv().appendChild(h4)
 }
@@ -50,33 +50,44 @@ const loadList = event => {
     const div1 = document.createElement('div')
     const label = document.createElement('label')
     const input = document.createElement('input')
-    const div2 = createSelectField('category', 'Category', 'Category', ['Vegetable', 'Meat', 'Dairy', 'Grain', 'Misc'] )
-    
+    const div2 = createSelectField('category', 's6', 'Category', 'Category', ['Vegetable', 'Meat', 'Dairy', 'Grain', 'Misc'])
+    const submit = document.createElement('submit')
+
 
 
     h1.innerText = 'Your Shopping List'
     p.innerText = 'enter your items and their categories below:'
-    
-    
+
+
     rowDiv.className = 'row'
 
     div1.className = 'input-field col s6'
     input.setAttribute('type', 'text')
     input.setAttribute('id', 'grocery item')
-    
+
     label.setAttribute('for', 'grocery item')
     label.innerText = "Grocery Item"
 
-    
+    submit.setAttribute('type', 'submit')
+    submit.className = 'btn orange lighten-2'
+    submit.innerText = 'Add to list'
+
+
     div1.appendChild(input)
     div1.appendChild(label)
-    
+
     rowDiv.appendChild(div1)
     rowDiv.appendChild(div2)
     form.appendChild(rowDiv)
+    form.appendChild(submit)
     mainDiv().appendChild(h1)
     mainDiv().appendChild(p)
     mainDiv().appendChild(form)
+
+    //initializer for materialize form input javascript
+    $(document).ready(function () {
+        $('select').formSelect();
+    });
 
 }
 
@@ -88,20 +99,20 @@ const loadMeals = event => {
     const button = document.createElement('a')
     const div = document.createElement('div')
     const randomMealDiv = document.createElement('div')
-    
+
 
     div.className = "center-align"
     randomMealDiv.className = "col s12 m7"
-    
+
     h1.innerText = "Can't decide what to eat?"
 
     button.className = "waves-effect waves-light btn orange lighten-2"
     button.innerText = "Click Here"
 
-    randomMealDiv.setAttribute('id','random')
+    randomMealDiv.setAttribute('id', 'random')
 
     button.addEventListener('click', getRandomMeal)
-    
+
     div.appendChild(h1)
     div.appendChild(button)
 
@@ -125,7 +136,7 @@ const loadRandomMeal = (meal) => {
 
     header.className = "header"
     header.innerText = 'You should make:'
-    
+
     horizontalDiv.className = "card horizontal"
 
     imageDiv.className = "card-image"
@@ -159,48 +170,49 @@ const loadRandomMeal = (meal) => {
 //Requests
 
 const getRandomMeal = () => {
-    const randomMeal = [Math.ceil(Math.random()*6)]
+    const randomMeal = [Math.ceil(Math.random() * 6)]
     fetch(baseUrl + '/meals' + "/" + randomMeal)
-    .then(response => response.json())
-    .then(meal => loadRandomMeal(meal))
+        .then(response => response.json())
+        .then(meal => loadRandomMeal(meal))
 }
 
 //NODE creators
 
 const createColumn = columnSize => {
     const div = document.createElement('div')
-    div.className = 'input-field col' + columnSize
+    div.className = 'input-field col ' + columnSize
     return div
 }
 
-const createSelectField = (id, placeholder, labelText, options=[]) => {
-    const div = createColumn('s6')
-    const select = document.createElement('select')
-    const option = document.createElement('option')
-    const label = document.createElement('label')
+const createSelectField = (id, colSize, placeholder, labelText, options = []) => {
+    const div = createColumn(colSize);
+    const select = document.createElement('select');
+    const option = document.createElement('option');
+    const label = document.createElement('label');
 
-    select.setAttribute('id', id)
+    select.setAttribute('id', id);
+    option.setAttribute('disabled', true);
+    option.setAttribute('selected', true);
+    option.value = '';
+    option.innerText = placeholder;
+    label.setAttribute('id', id);
+    label.innerText = labelText;
 
-    option.setAttribute('disabled', true)
-    option.setAttribute('selected', true)
-    option.value = ''
-    option.innerText = placeholder
-    select.appendChild(option)
-
-    label.setAttribute('id', id)
-    label.innerText = labelText
-
+    select.appendChild(option);
 
     options.forEach(optionText => {
-        const option = document.createElement('option')
-        option.innerText = optionText
-        option.value = optionText
-        select.appendChild(option)
-    })    
+        const option = document.createElement('option');
+        option.innerText = optionText;
+        option.value = optionText;
+        select.appendChild(option);
+    })
 
-    div.appendChild(select)
-    div.appendChild(label)
+    div.appendChild(select);
+    div.appendChild(label);
+
+    return div;
 }
+
 
 
 
@@ -212,7 +224,7 @@ const resetMainDiv = () => {
 
 
 //WHEN THE DOM LOADS
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     //load the home page
     loadHome()
     attachHomePageLinkEvent()
