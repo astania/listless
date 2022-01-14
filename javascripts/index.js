dd9f9ee6d924bd536d007d66649f301dfef1087a
+ <+>
2022-01-14 14:07:07 -0500

    final touches

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,11 +1,26 @@
 //Globals
-const baseUrl = "http://localhost:3000"
+const baseUrl = 'http://localhost:3000'
+
+
 
 //Node getters
 const mainDiv = () => document.getElementById('main')
 const homeLink = () => document.getElementById('home-link')
 const listLink = () => document.getElementById('list-link')
 const mealLink = () => document.getElementById('meal-link')
+const groceryItem = () => document.getElementById('grocery-item')
+const category = () => document.getElementById('category')
+const shoppingListDiv = () => document.getElementById('list-div')
+const vegetableCategory = () => document.getElementById('category-vegetable')
+const meatCategory = () => document.getElementById('category-meat')
+const dairyCategory = () => document.getElementById('category-dairy')
+const grainCategory = () => document.getElementById('category-grain')
+const miscCategory = () => document.getElementById('category-misc')
+const tryAgainBtn = () => document.getElementById('try-again')
+const logoHomeLink = () => document.getElementById('logo-home')
+const randomBtn = () => document.getElementById('random-button')
+
+
 
 //Event Listeners
 
@@ -20,75 +35,153 @@ const attachListLinkEvent = () => {
 const attachMealLinkEvent = () => {
     mealLink().addEventListener('click', loadMeals)
 }
+
+const attachHomeLinkEvent = () => {
+    logoHomeLink().addEventListener('click', loadHome)
+}
+
+
 //Event Handlers
 
 const loadHome = event => {
     if (event) {
         event.preventDefault()
     }
+
     resetMainDiv()
     const h1 = document.createElement('h1')
     const h4 = document.createElement('h4')
+    const shoppingIcon = document.createElement('i')
 
-    h1.className = "center-align"
-    h4.className = "center-align container"
+    h1.className = 'center-align'
+    h4.className = 'center-align container'
+    shoppingIcon.className = 'large material-icons'
+    shoppingIcon.style.verticalAlign = 'middle'
     h1.innerText = 'Welcome to LISTLESS'
-    h4.innerText = "Listless allows you to sort your shopping list into categories, so you won't leave the dairy section without grabbing oat milk ever again!"
-
+    h4.innerText = 'Never leave the dairy section without grabbing oat milk ever again!'
+    shoppingIcon.textContent = 'shopping_cart'
 
     mainDiv().appendChild(h1)
     mainDiv().appendChild(h4)
+    h1.appendChild(shoppingIcon)
 }
 
 const loadList = event => {
     event.preventDefault()
     resetMainDiv()
-    const h1 = document.createElement('h1')
+    const h2 = document.createElement('h2')
     const p = document.createElement('p')
+    const topHeading = document.createElement('h4')
     const form = document.createElement('form')
+    const listDiv = document.createElement('div')
     const rowDiv = document.createElement('div')
     const div1 = document.createElement('div')
     const label = document.createElement('label')
     const input = document.createElement('input')
-    const div2 = createSelectField('category', 's6', 'Category', 'Category', ['Vegetable', 'Meat', 'Dairy', 'Grain', 'Misc'])
-    const submit = document.createElement('submit')
+    const div2 = createSelectField('category', 's3', 'Category', 'Category', ['Vegetable', 'Meat', 'Dairy', 'Grain', 'Misc'])
+    const submit = document.createElement('button')
+    const vegetable = document.createElement('h6')
+    const meat = document.createElement('h6')
+    const dairy = document.createElement('h6')
+    const grain = document.createElement('h6')
+    const misc = document.createElement('h6')
 
+    listDiv.setAttribute('id', 'list-div')
 
+    topHeading.innerText = 'Shopping List'
+    topHeading.className = 'center-align'
 
-    h1.innerText = 'Your Shopping List'
+    h2.innerText = 'Add Items to the Shopping List'
+    h2.className = 'center-align'
     p.innerText = 'enter your items and their categories below:'
-
+    p.className = 'center-align'
 
     rowDiv.className = 'row'
 
     div1.className = 'input-field col s6'
     input.setAttribute('type', 'text')
-    input.setAttribute('id', 'grocery item')
+    input.setAttribute('id', 'grocery-item')
 
     label.setAttribute('for', 'grocery item')
-    label.innerText = "Grocery Item"
+    label.innerText = 'Grocery Item'
 
     submit.setAttribute('type', 'submit')
-    submit.className = 'btn orange lighten-2'
+    submit.setAttribute('id', 'submit-form')
+    submit.style.display = 'table-row'
+    submit.className = 'btn waves-effect waves-light orange lighten-2'
     submit.innerText = 'Add to list'
 
+    vegetable.innerText = 'Vegetables'
+    vegetable.setAttribute('id', 'category-vegetable')
+    meat.innerText = 'Meats'
+    meat.setAttribute('id', 'category-meat')
+    dairy.innerText = 'Dairy'
+    dairy.setAttribute('id', 'category-dairy')
+    grain.innerText = 'Grain'
+    grain.setAttribute('id', 'category-grain')
+    misc.innerText = 'Misc.'
+    misc.setAttribute('id', 'category-misc')
 
     div1.appendChild(input)
     div1.appendChild(label)
-
     rowDiv.appendChild(div1)
     rowDiv.appendChild(div2)
     form.appendChild(rowDiv)
     form.appendChild(submit)
-    mainDiv().appendChild(h1)
+    listDiv.appendChild(topHeading)
+    listDiv.appendChild(vegetable)
+    listDiv.appendChild(meat)
+    listDiv.appendChild(dairy)
+    listDiv.appendChild(grain)
+    listDiv.appendChild(misc)
+    mainDiv().appendChild(h2)
     mainDiv().appendChild(p)
     mainDiv().appendChild(form)
+    mainDiv().appendChild(listDiv)
+    form.addEventListener('submit', submitForm)
 
     //initializer for materialize form input javascript
     $(document).ready(function () {
         $('select').formSelect();
     });
+}
 
+const submitForm = event => {
+    event.preventDefault()
+    const ul = document.createElement('ul')
+    const shoppingItem = document.createElement('li')
+    const deleteButton = document.createElement('button')
+
+    shoppingItem.innerText = capitalizeFirstLetter(groceryItem().value)
+    shoppingItem.style.listStyle = 'circle'
+    shoppingItem.style.marginLeft = '50px'
+
+    deleteButton.textContent = 'x'
+    deleteButton.setAttribute('class', 'waves-effect waves-light btn-small')
+    deleteButton.style.marginLeft = '8px'
+    deleteButton.addEventListener('click', deleteItem)
+
+    shoppingItem.appendChild(deleteButton)
+    ul.appendChild(shoppingItem)
+
+    const selectorValue = category().value
+
+    if (selectorValue === 'Vegetable') {
+        vegetableCategory().appendChild(ul)
+    } else if (selectorValue === 'Meat') {
+        meatCategory().appendChild(ul)
+    } else if (selectorValue === 'Dairy') {
+        dairyCategory().appendChild(ul)
+    } else if (selectorValue === 'Grain') {
+        grainCategory().appendChild(ul)
+    } else if (selectorValue === 'Misc') {
+        miscCategory().appendChild(ul)
+    }
+}
+
+
+const deleteItem = event => {
+    event.target.parentNode.remove()
 }
 
 const loadMeals = event => {
@@ -100,14 +193,14 @@ const loadMeals = event => {
     const div = document.createElement('div')
     const randomMealDiv = document.createElement('div')
 
+    div.className = 'center-align'
+    randomMealDiv.className = 'col s12 m7'
 
-    div.className = "center-align"
-    randomMealDiv.className = "col s12 m7"
+    h1.innerText = 'Can\'t decide what to eat?'
 
-    h1.innerText = "Can't decide what to eat?"
-
-    button.className = "waves-effect waves-light btn orange lighten-2"
-    button.innerText = "Click Here"
+    button.className = 'waves-effect waves-light btn orange lighten-2'
+    button.setAttribute('id', 'random-button')
+    button.innerText = 'Click Here'
 
     randomMealDiv.setAttribute('id', 'random')
 
@@ -120,9 +213,11 @@ const loadMeals = event => {
     mainDiv().appendChild(randomMealDiv)
 }
 
+
 const loadRandomMeal = (meal) => {
     const randomMealDiv = document.getElementById('random')
     randomMealDiv.innerHTML = ''
+    randomBtn().innerText = 'Try again?'
     const header = document.createElement('h2')
     const horizontalDiv = document.createElement('div')
     const imageDiv = document.createElement('div')
@@ -132,26 +227,26 @@ const loadRandomMeal = (meal) => {
     const content = document.createElement('p')
     const actionDiv = document.createElement('div')
     const mealLink = document.createElement('a')
-    const linkText = document.createTextNode("Get the Recipe")
+    const linkText = document.createTextNode('Get the Recipe')
 
-    header.className = "header"
+    header.className = 'header'
     header.innerText = 'You should make:'
 
-    horizontalDiv.className = "card horizontal"
+    horizontalDiv.className = 'card horizontal card small'
 
-    imageDiv.className = "card-image"
+    imageDiv.className = 'card-image'
     mealPicture.src = meal.image
 
+    content.className = 'card-title'
     content.innerText = meal.meal
 
     mealLink.href = meal.link
-    mealLink.title = "Get the Recipe"
-
+    mealLink.title = 'Get the Recipe'
+    mealLink.setAttribute('target', '_blank')
 
     actionDiv.className = 'card-action'
     contentDiv.className = 'card-content'
     stackedDiv.className = 'card-stacked'
-
 
     mealLink.appendChild(linkText)
     actionDiv.appendChild(mealLink)
@@ -161,20 +256,22 @@ const loadRandomMeal = (meal) => {
     contentDiv.appendChild(content)
     stackedDiv.appendChild(contentDiv)
     stackedDiv.appendChild(actionDiv)
+    horizontalDiv.appendChild(imageDiv)
     horizontalDiv.appendChild(stackedDiv)
     randomMealDiv.appendChild(header)
     randomMealDiv.appendChild(horizontalDiv)
-
 }
+
 
 //Requests
 
 const getRandomMeal = () => {
-    const randomMeal = [Math.ceil(Math.random() * 6)]
-    fetch(baseUrl + '/meals' + "/" + randomMeal)
+    const randomMeal = Math.ceil(Math.random() * 6)
+    fetch(baseUrl + '/meals' + '/' + randomMeal)
         .then(response => response.json())
         .then(meal => loadRandomMeal(meal))
 }
+
 
 //NODE creators
 
@@ -214,18 +311,19 @@ const createSelectField = (id, colSize, placeholder, labelText, options = []) =>
 }
 
 
-
-
 //Misc
 
 const resetMainDiv = () => {
     mainDiv().innerHTML = ''
 }
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 
 //WHEN THE DOM LOADS
 document.addEventListener('DOMContentLoaded', function () {
-    //load the home page
     loadHome()
     attachHomePageLinkEvent()
     attachListLinkEvent()
